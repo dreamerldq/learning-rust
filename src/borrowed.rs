b0096950097f8597fcdb9b286ecb4ffd37afc55e
dreamerldq <lidanqiu96@foxmail.com>
2022-03-23 23:18:27 +0800

    所有权和借用

--- src/borrowed.rs
+++ src/borrowed.rs
@@ -48,4 +48,12 @@ fn main() {
     
     p.push_str("world");
 }
+// 练习8
+fn main() {
+    // 通过修改下面一行代码来修复错误
+    let mut s = String::from("hello, ");
 
+    borrow_object(&mut s)
+}
+
+fn borrow_object(s: &mut String) {}
