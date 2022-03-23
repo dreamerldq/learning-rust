943be1ad653a805b048e5123aa95644eaba1f0c7
dreamerldq <lidanqiu96@foxmail.com>
2022-03-23 23:17:41 +0800

    所有权和借用

---
+++ src/borrowed.rs
@@ -0,0 +1,51 @@
+// 练习1
+fn main() {
+    let x = 5;
+    // 填写空白处
+    let p = &x;
+ 
+    println!("x 的内存地址是 {:p}", p); // output: 0x16fa3ac84
+ }
+
+// 练习2
+
+fn main() {
+    let x = 5;
+    let y = &x;
+
+    // 只能修改以下行
+    assert_eq!(5, *y);
+}
+
+
+// 练习3
+fn main() {
+    let mut s = String::from("hello, ");
+
+    borrow_object(&mut s)
+}
+
+fn borrow_object(s: &String) {}
+
+
+// 练习4
+fn main() {
+    let mut s = String::from("hello, ");
+
+    push_str(&mut s)
+}
+
+fn push_str(s: &mut String) {
+    s.push_str("world")
+}
+// 练习5
+
+fn main() {
+    let mut s = String::from("hello, ");
+
+    // 填写空白处，让代码工作
+    let p = &mut s;
+    
+    p.push_str("world");
+}
+
--- src/owner.rs
+++ src/owner.rs
@@ -1,9 +1,56 @@
+//练习1
 fn main() {
-    // greet_world()
-    increase();
-    let (x, y) = (1, 2);
-    let s = sum(x, y);
-    print();
-    never_return();
-    assert_eq!(s, 3);
+    // 使用尽可能多的方法来通过编译
+    let x = String::from("hello, world");
+    let y = x.clone();
+    println!("{},{}",x,y);
+}
+
+//练习2
+fn main() {
+    let s1 = String::from("hello, world");
+    let s2 = take_ownership(s1);
+
+    println!("{}", s2);
+}
+
+// 只能修改下面的代码!
+fn take_ownership(s: String) -> String {
+    println!("{}", s);
+    s
+}
+
+
+// 练习4
+fn main() {
+    let mut s = String::from("hello, world");
+
+    s = print_str(s);
+
+    println!("{}", s);
+}
+
+fn print_str(s: String)->String  {
+    println!("{}",s);
+    s
+}
+
+// 练习5   由基本类型构成的元组也具备copy属性
+// 不要使用 clone，使用 copy 的方式替代
+fn main() {
+    let x = (1, 2, (), "hello");
+    let y = x;
+    println!("{:?}, {:?}", x, y);
+}
+
+
+// 练习6 当所有权转移时，可变性也可以随之改变。
+fn main() {
+    let s = String::from("hello, ");
+    
+    // 只修改下面这行代码 !
+    let mut s1 = s;
+
+    s1.push_str("world");
+    println!("{}", s1);
 }
